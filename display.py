1d05e7286ecd5b7e734d85aeec0b8b2dd1052b13
kiraninsofe <92089283+kiraninsofe@users.noreply.github.com>
2021-10-22 14:40:56 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,8 +1,8 @@
-list-1 = [10,20,30,40,50]
+list_1 = [10,20,30,40,50]
 res_list = [x^2 for x in list-1]
 print(res_list)
 
-list-2 = [101,202,303,404,505]
+list_2 = [101,202,303,404,505]
 sub_list = [x*0.1 for x in list-2]
 print(sub_list)
 
